202315a5c06e4d1a6da3b0faca4c4306e84945de
panyaxbo <panya.javamania@gmail.com>
2018-02-02 16:20:39 +0700

    first commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, facilis sed. Amet sequi laboriosam doloremque eos quaerat nemo officia voluptate mollitia unde animi eius vel, sunt deleniti. Quis, eaque hic.',
+            'author': 'Panya Boonyakulsrirung',
+            'create_date': '30-01-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, facilis sed. Amet sequi laboriosam doloremque eos quaerat nemo officia voluptate mollitia unde animi eius vel, sunt deleniti. Quis, eaque hic.',
+            'author': 'John Doe',
+            'create_date': '04-01-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, facilis sed. Amet sequi laboriosam doloremque eos quaerat nemo officia voluptate mollitia unde animi eius vel, sunt deleniti. Quis, eaque hic.',
+            'author': 'John Doe',
+            'create_date': '15-10-2017'
+        }
+    ]
+    return articles
+
